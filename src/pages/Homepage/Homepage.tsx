5e6291f9bb15ab9d02a70f259e97e56c4e20975e
guille-gallo <guille.gallo88@gmail.com>
2026-01-11 16:06:55 +0100

    test actions

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -41,7 +41,7 @@ export const Homepage: React.FC = () => {
         
         <div className="homepage__section">
           <Carousel
-            category={MOVIE_CATEGORIES[0]} // Popular
+            category={MOVIE_CATEGORIES[0]} // Popular Movies
             movies={popularMovies.data?.results || []}
             loading={popularMovies.isLoading}
             error={popularMovies.error?.message || null}
